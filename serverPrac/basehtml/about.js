496b01266710f3f9cf81ddd559e1753b04f37e61
leesemin <leesemin222@gmail.com>
2023-03-29 19:01:46 +0900

    간단한 입력 폼 만듦

--- serverPrac/basehtml/about.js
+++ serverPrac/basehtml/about.js
@@ -3,12 +3,13 @@ import tagMakeCall from "../modules/tagMakeCall.js"
 
 const root = document.getElementById('root')
 const rootChild = ['header','main','footer']
+const users = [];
 
 for(let i=0 ; i<rootChild.length;i++){
   tagMakeCall(root, rootChild[i],function(element){
     element.style.border = '2px solid blue'
-    element.style.width = '100px'
-    element.style.height = '100px'
+    element.style.width = '40vw'
+    element.style.height = '25vh'
   })
 }
 
@@ -24,3 +25,40 @@ tagMakeCall(aboutForm,'input',function(element){
   element.setAttribute('type','submit')
   element.setAttribute('id','about-btn')
 })
+
+const header = document.querySelector('header')
+const main = document.querySelector('main')
+const footer = document.querySelector('footer')
+
+tagMakeCall(header,'form',function(element){
+  element.setAttribute('id','prolog-users')
+  element.setAttribute('method','post')
+})
+
+const prologUsers = document.getElementById('prolog-users')
+
+for(let i = 0 ; i < 5 ; i++){
+  tagMakeCall(prologUsers,'div',function(){})
+}
+
+for(let i = 0 ; i < 5 ; i++){
+  tagMakeCall(prologUsers.children[i],'div',function(){})
+  tagMakeCall(prologUsers.children[i],'input',function(element){
+      element.setAttribute('type','text')
+    }//콜백 끝
+    )//else tagmakecall 끝
+}
+
+let divInnerText = ['name : ','birth_date : ','married : ','comment : ','nickname : ']
+
+
+
+for(let i=0;i<divInnerText.length;i++){
+  prologUsers.children[i].children[0].innerText = divInnerText[i]
+}
+
+tagMakeCall(prologUsers,'button',function(element){
+  element.setAttribute('type','submit')
+  element.innerText = '버튼을 누르시오'
+  })
+
